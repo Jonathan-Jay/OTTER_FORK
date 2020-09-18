8f5bfe05e74ba90ec3a44b558647a5ff2bafd4d3
Jonathan Jay <jonathan.jay@ontariotechu.net>
2020-09-18 17:09:11 -0400

    Update main.cpp

--- projects/Graphics tut 1/src/main.cpp
+++ projects/Graphics tut 1/src/main.cpp
@@ -2,6 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+class vec3 {
+public:
+	vec3(float x_, float y_, float z_) : x(x_), y(y_), z(z_) {}
+	float x = 0, y = 0, z = 0;
+
+	vec3 operator+(const vec3& other) { return vec3(this->x + other.x, this->y + other.y, this->z + other.z); }
+	vec3 operator-(const vec3& other) { return vec3(this->x - other.x, this->y - other.y, this->z - other.z); }
+	vec3 operator*(float f) { return vec3(this->x * f, this->y * f, this->z * f); }
+};
+
+vec3 LERP(vec3 p0, vec3 p1, float t) {
+	return p0 * (1.f - t) + p1 * t;
+}
+
 int main() {
 	// Initialize GLFW
 	if (glfwInit() == GLFW_FALSE) {
@@ -23,13 +37,26 @@ int main() {
 	std::cout << glGetString(GL_RENDERER) << std::endl;
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	vec3 p0(0, 0, 0);
+	vec3 p1((rand() % 11) / 10, (rand() % 11) / 10, (rand() % 11) / 10);
+
+	float t(0);
+
 	// Run as long as the window is open
 	while (!glfwWindowShouldClose(window)) {
 		// Poll for events from windows (clicks, keypressed, closing, all that)
 		glfwPollEvents();
 
+		t += 0.001;
+		if (t >= 1) {
+			t = 0;
+			p0 = p1;
+			p1 = vec3((rand() % 11) / 10.f, (rand() % 11) / 10.f, (rand() % 11) / 10.f);
+		}
+
+		vec3 temp = LERP(p0, p1, t);
 		// Clear our screen every frame
-		glClearColor((rand() % 11) / 10.f, (rand() % 11) / 10.f, (rand() % 11) / 10.f, (rand() % 11) / 10.f);
+		glClearColor(temp.x, temp.y, temp.z, 1);
 		//glClearColor(float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX);
 		glClear(GL_COLOR_BUFFER_BIT);
 
